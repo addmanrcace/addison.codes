0933aee8ee391e56309a33d11d2fe977c9479276
Addison Wanlass <addmanrcace@gmail.com>
2020-08-20 00:00:57 -0400

    Fixed styling

--- abl/src/components/books/CollectionBookCard.js
+++ abl/src/components/books/CollectionBookCard.js
@@ -6,12 +6,19 @@ const BookCard = props => {
 
   const { deleteBook, updateBook } = bookContext;
 
-  const { _id, title, author, averageRating, publishYear, photo, status } = props.book;
+  const { _id, author, averageRating, publishYear, photo, status } = props.book;
+
+  let { title } = props.book;
 
   const book = {
     _id,
     status,
   };
+
+  if (title.length > 20) {
+    const titleArray = title.split(' ').slice(0, 5);
+    title = titleArray.join(' ') + '...';
+  }
 
   const onSubmit = e => {
     e.preventDefault();
@@ -32,7 +39,7 @@ const BookCard = props => {
   };
 
   return (
-    <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 overflow-hidden">
+    <div className="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/3 p-4 overflow-hidden">
       <div className="w-full h-full bg-white rounded overflow-hidden shadow-lg flex flex-row">
         <img style={imgStyle} className="" src={photo} alt="Display" />
         <div className="">
@@ -57,26 +64,21 @@ const BookCard = props => {
             </div>
           </div>
           <div className="px-2 py-1">
-            <button className="px-3 py-2 bg-blue-700 rounded text-white" onClick={onSubmit}>
+            <button className="px-3 py-2 mx-2 my-2 bg-blue-700 rounded text-white" onClick={onSubmit}>
               Delete Book
             </button>
             {/* This works poorly, should come from response */}
             <div className="inline-block relative w-64">
               <select
-                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                className="block mx-2 my-2 appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 onChange={onChange}
               >
-                <option value={null}></option>
+                <option value={null}>Change status</option>
                 <option value="Want to read">Want to read</option>
                 <option value="Reading">Reading</option>
                 <option value="Dropped">Dropped</option>
                 <option value="Finished">Finished</option>
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
             </div>
           </div>
         </div>
